5145e95380a1a06b2bf2088584962c2b877e8449
mgoov56 <102950854+mgoov56@users.noreply.github.com>
2022-10-20 16:34:18 +0200

    [add] add, modif, delete, annul, sortie ok

--- public/js/sortie.js
+++ public/js/sortie.js
@@ -1,7 +1,37 @@
-function prepareFormLieu(){
+function hideFormLieu(){
+    $('.f_lieu').removeAttr('required');
+    $('div:has(> .f_lieu)').hide();
+    $('.btn_add_lieu').show();
+}
 
+function showFormLieu(){
+    $('div:has(> .f_lieu)').show();
+    $('.f_lieu').attr('required', 'required');
+    $('.btn_add_lieu').hide();
+}
+
+function setNewListLieu(){
+    if($('#sortie_lieu option[value="0"]').length === 0){
+        $('#sortie_lieu').append($('<option value="0" selected>new</option>'));
+    }
+}
+
+function removeNewListLieu(){
+    $('#sortie_lieu option[value="0"]').remove();
 }
 
 function sortie(){
-    console.log($('#btn_add_lieu'));
+
+    removeNewListLieu();
+    hideFormLieu();
+
+    $('#sortie_lieu').change(function() {
+        hideFormLieu();
+        removeNewListLieu();
+    });
+
+    $('.btn_add_lieu').click(function() {
+        showFormLieu();
+        setNewListLieu();
+    });
 }
