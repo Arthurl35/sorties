4a7205720952c9587828c4073632c1efa24d377d
alevesque2021 <arthur.levesque35@gmail.com>
2022-10-27 09:49:52 +0200

    modification sauvegarde photo de profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -43,7 +43,7 @@ class ProfilController extends AbstractController
                 //gestion de l'upload de l'image
                 $backdrop = $participantForm->get('backdrop')->getData();
                 if($backdrop) {
-                    $participant->setBackdrop($upload->saveFile($backdrop, $participant->getNom(), $this->getParameter('upload_directory')));
+                    $participant->setBackdrop($upload->saveFile($backdrop, $participant->getNom(), $this->getParameter('sorties_backdrop_dir')));
                 }
 
                 //enregistrement des données
